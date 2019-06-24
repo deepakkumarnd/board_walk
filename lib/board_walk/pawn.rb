82202d02e4de51ebb1ff4ed835e3ed7a3c8ab1ed
deepak.kumar <deepak.kumar@truecaller.com>
2019-06-24 14:52:50 +0530

    cleanups

--- lib/board_walk/pawn.rb
+++ lib/board_walk/pawn.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 module BoardWalk
 
+  Move = Struct.new(:xdiff, :ydiff)
+
   class Pawn
 
     FREEDOM_OF_MOVES = [
@@ -13,14 +15,15 @@ module BoardWalk
       [2, -2],
       [0, -3],
       [-2, -2]
-    ].freeze
+    ].map { |xdiff, ydiff| Move.new(xdiff, ydiff) }
+     .freeze
 
     attr_reader :path
 
     def initialize(board, initial_x, intial_y)
       @board = board
       raise OutOfBoardError if !@board.within_board?(initial_x, intial_y)
-      @start_cell   = @board.cell(initial_x, intial_y)
+      @start_cell = @board.cell(initial_x, intial_y)
       @path = []
     end
 
@@ -37,8 +40,7 @@ module BoardWalk
             print_path
             break
           else
-            # binding.pry
-            c  = @path.pop
+            c = @path.pop
             c.unvisit! if c
             @current_cell = @path.pop
             if @current_cell.nil?
@@ -57,7 +59,7 @@ module BoardWalk
     end
 
     def find_best_move
-      valid_moves(@current_cell)
+      valid_moves_from(@current_cell)
         .min_by { |cell| score(cell) }
     end
 
@@ -72,27 +74,27 @@ module BoardWalk
     def print_path
       puts "\nFound path"
       path_str = @path.map do |cell|
-        "(#{cell.x}, #{cell.y})"
+        cell.address
       end.join(' -> ')
 
       puts path_str
       puts "\n"
     end
 
-    def valid_moves(cell)
+    def valid_moves_from(cell)
       possible_moves_from(cell.x, cell.y)
         .map { |x,y| @board.cell(x, y) }
         .select { |cell| !cell.visited? }
     end
 
     def possible_moves_from(x, y)
-      FREEDOM_OF_MOVES.map do |i,j|
-        valid_cell_position?(x + i, y + j) ? [x + i, y + j] : nil
+      FREEDOM_OF_MOVES.map do |move|
+        [x + move.xdiff, y + move.ydiff] if valid_cell_position?(x + move.xdiff, y + move.ydiff)
       end.compact
     end
 
     def score(cell)
-      valid_moves(cell).length
+      valid_moves_from(cell).length
     end
 
     def valid_cell_position?(x, y)
